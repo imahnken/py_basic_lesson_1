2b7950f088ee0de5f3a5fd29b17c4fd9bc36fb57
Igor <igor.mahnken@yahoo.com>
2020-02-26 17:23:49 -0500

    tasks 3-6 Branch task_1

---
+++ task_3.py
@@ -0,0 +1,4 @@
+n = int(input('Введите целое число n от 0 до 9: '))
+# result = n + n + n * 10 + n + n * 10 + n * 100
+result = n * 3 + n * 20 + n * 100
+print(f'Сумма чисел n + nn + nnn равна: {result}')
