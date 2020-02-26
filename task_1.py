d2d1a945cc0edd26821692dfa1aa6e0bab73968c
Igor <igor.mahnken@yahoo.com>
2020-02-26 17:21:34 -0500

    task_1 Branch task_1

---
+++ task_1.py
@@ -0,0 +1,9 @@
+a = 5
+b = True
+c = 'hello'
+d = 3.1415
+print(a, b, c, d)
+name = input('Введите ваше имя: ')
+age = input('Введите ваш возраст: ')
+city = input('Напишите ваш город: ')
+print(f'Пользователь {name} из города {city} - возраст {age}')
